e98ed6fe2887dbe0f4d03d9058d49865e1d40ad5
KatsumiYokota <katsumi.yokota@shimoning.com>
2023-05-19 11:15:09 +0900

    Add login function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// お問合せフォーム
-Route::get('/contact/create','App\Http\Controllers\ContactController@create')->name('contact.create'); // ページを表示
-Route::post('/contact/store','App\Http\Controllers\ContactController@store')->name('contact.store'); // 内容を保存
-Route::get('/contact/index', 'App\Http\Controllers\ContactController@index')->name('contact.index'); // 一覧を表示
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('/post','App\Http\Controllers\PostController'); 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
