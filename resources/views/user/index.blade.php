e699fd70ddad82a1aacbeca02baeaadf2ce06051
KatsumiYokota <katsumi.yokota@shimoning.com>
2023-05-25 11:47:38 +0900

    Add comments

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,7 +9,7 @@
         <th>@sortablelink('password', 'パスワード')</th>
         <th>詳細</th>
         <th>編集</th>
-        {{-- <th>削除</th> --}}
+        <th>削除</th>
       </tr>
     </thead>
     <tbody>
@@ -20,15 +20,15 @@
         <td>{{ $user->password }}</td>
         <td><a href="{{ route('user.show', $user->id) }}" class="btn btn-secondary">詳細</a></td>
         <td><a href="{{ route('user.edit', $user->id) }}" class="btn btn-primary">編集</a></td>
-        {{-- <td>
+        <td>
           <form method="post" action="{{ route('user.destroy', $user->id) }}" class="ms-1">
             @csrf
             @method('delete') <!-- 削除用 -->
-            <button type="submit" class="btn btn-danger" onClick="return confirm('本当に削除しますか？');"> <!-- jsで確認 -->
+            <button type="submit" class="btn btn-warning" onClick="return confirm('本当に削除しますか？');"> <!-- jsで確認 -->
               削除
             </button>
           </form>
-        </td> --}}
+        </td>
       </tr>
       @endforeach
     </tbody>
