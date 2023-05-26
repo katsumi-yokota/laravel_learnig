c6933b9eaccba0f55aa76648c7a363bd7e93952e
KatsumiYokota <katsumi.yokota@shimoning.com>
2023-05-26 17:36:14 +0900

    Delete comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,43 +21,6 @@ class UserController extends Controller
         $users = User::query()->sortable()->withTrashed()->paginate(5); // 論理削除されたユーザーも取得、表示
 
         return view('user.index', ['users' => $users, 'sort' => $sort]);
-
-
-        // // ユーザー一覧をページネートで取得
-        // $users = User::paginate(20);
-
-        // // 検索フォームで入力された値を取得する
-        // $search = $request->input('search');
-
-        // // クエリビルダ
-        // $query = User::query();
-
-        // // もし検索フォームにキーワードが入力されたら
-        // if ($search) {
-
-        //     // 全角スペースを半角に変換
-        //     $spaceConversion = mb_convert_kana($search, 's');
-
-        //     // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
-        //     $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
-
-
-        //     // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
-        //     foreach($wordArraySearched as $value) {
-        //         $query->where('name', 'like', '%'.$value.'%');
-        //     }
-
-        //     // 上記で取得した$queryをページネートにし、変数$usersに代入
-        //     $users = $query->paginate(5);
-        
-        //         }
-        
-        //         // ビューにusersとsearchを変数として渡す
-        //         return view('users.index')
-        //             ->with([
-        //                 'users' => $users,
-        //                 'search' => $search,
-        //             ]);
         }
 
     /**
