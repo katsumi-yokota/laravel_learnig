303507a8686eddc5b8304e23b4816e4651d7ed21
KatsumiYokota <katsumi.yokota@shimoning.com>
2023-05-30 19:11:33 +0900

    Modify alert messages

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -2,18 +2,21 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-@if(session('message'))
-<div class="alert alert-success">{{  session('message')  }}</div>
-@endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    @if(session('succeed'))
+    <div class="alert alert-success">{{  session('succeed')  }}</div>
+    @endif
+    @if(session('warning'))
+    <div class="alert alert-danger">{{  session('warning')  }}</div>
+    @endif
 <div class="row">
     <div class="col-10 col-md-8 col-lg-6 mx-auto mt-6">
         <div class="card-body">
