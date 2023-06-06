5b9342fab01e49439c64e4cbb3b101a9556cf33d
KatsumiYokota <katsumi.yokota@shimoning.com>
2023-06-06 11:30:02 +0900

    Fix nest

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -27,7 +27,7 @@
                     <label for="contact_category_id">お問合せカテゴリー</label>
                     <select name="contact_category_id" id="contact_category_id" class="form-control" value="{{  old('name')  }}">
                         @foreach ($contactCategories as $contactCategory)
-                        <option value="{{ $contactCategory->id }}">{{ $contactCategory->name }}</option>
+                            <option value="{{ $contactCategory->id }}">{{ $contactCategory->name }}</option>
                         @endforeach
                     </select>
                 </div>
